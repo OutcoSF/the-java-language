a0922ad8c079ee10c8ff6105eab6fb28e67cfff1
Alexander Saavedra <awsaavedra@gmail.com>
2019-06-06 17:06:37 -0700

    producers, consumers, and unary operators

--- src/com/company/functional_programming/Functions.java
+++ src/com/company/functional_programming/Functions.java
@@ -1,3 +1,8 @@
+/*
+
+    For more information please see: https://docs.oracle.com/javase/8/docs/api/java/util/function/package-summary.html
+ */
+
 package com.company.functional_programming;
 
 import com.company.object_oriented_programming.Musician;
@@ -5,8 +10,7 @@ import com.company.object_oriented_programming.Musician;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class Functions {
     public static void main(String[] args) {
@@ -44,13 +48,36 @@ public class Functions {
                 System.out.println(getAName(getLastName, musician));
             }
         }
+
+        Function<Musician, String> lowerCase = musician -> musician.getName().toLowerCase();
+
+        Function<String, String> firstName = name -> name.substring(0, name.indexOf(' '));
+
+        Function chaindFunc = lowerCase.andThen(firstName);
+
+        System.out.println(chaindFunc.apply(musicians.get(0)));
+
+        BiFunction<String, Musician, String> concatAge = (String name, Musician musician) -> {
+            return name.concat(" " + musician.getAge());
+        };
+
+        String lowerName = lowerCase.apply(musicians.get(0));
+
+        DoubleUnaryOperator cremBy5 = k -> k + 20.0;
+        System.out.println(cremBy5.applyAsDouble(10.0));
+
+        // Do consumers return anything?
+        Consumer<String> consumer1 = str -> str.toLowerCase();
+        Consumer<String> consumer2 = str -> System.out.println(str);
+
+        consumer1.andThen(consumer2).accept("Whoopie!");
     }
 
     private static String getAName(Function<Musician, String> getName, Musician musician) {
         return getName.apply(musician);
     }
 
-    private static void printEmployeesByAge(List<Musician> musicians,
+    private static void printMusiciansByAge(List<Musician> musicians,
                                             String ageText,
                                             Predicate<Musician> ageCondition) {
 
